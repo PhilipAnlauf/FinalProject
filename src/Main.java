e81254ad357e26da5e8fa53a12c97ae1fbff6c14
philip.anlauf23 <soccerphil8@yahoo.com>
2023-02-01 11:21:17 -0600

    Added blue background. Added seconds main game panel, added character and 2nd Panel class.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,36 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Main {
     public static void main(String[] args) {
     JFrame gameScreen = new JFrame();
     gameScreen.setSize(750,750);
-
+    //==================================
     JPanel startMenu = new JPanel();
     startMenu.setBackground(Color.blue);
-    gameScreen.add(startMenu);
+    //==================================
+        JPanel mainGamePanel = new JPanel();
+        mainGamePanel.setBackground(Color.green);
+        //==================================
+
+        //Adding panels=====================
+        gameScreen.add(startMenu);
+        //==================================
+        startMenu.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if(e.getY()>=350 & e.getX()>=125 & e.getX()<=625){
+                    System.out.println("Game started");
+                    startMenu.setVisible(false);
+                    gameScreen.add(mainGamePanel);
+                }
+                else{
+                    System.out.println("Game not started");
+                }
+
+            }
+        });
 
     gameScreen.setVisible(true);
     }
