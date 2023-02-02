9851aff5974220561039b1e775388cc8c117e384
PhilipAnlauf <soccerphil8@yahoo.com>
2023-02-01 20:17:22 -0600

    added sout test added two panels, attempted adding start menu image

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,11 @@ public class Main {
     //==================================
         JPanel mainGamePanel = new JPanel();
         mainGamePanel.setBackground(Color.green);
-        //==================================
-
+        //Adding Start Menu image ==========
+        ImageIcon menuImage = new ImageIcon("StartMenuImage.bmp");
+        menuImage.setImage(menuImage.getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT));
+        JLabel startMenuImage = new JLabel();
+        startMenu.add(startMenuImage);
         //Adding panels=====================
         gameScreen.add(startMenu);
         //==================================
