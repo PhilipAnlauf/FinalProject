ebac29532f8e0bacc37e33c7a4b66923115149ab
PhilipAnlauf <soccerphil8@yahoo.com>
2023-02-12 22:29:09 -0600

    Initial Commit, M: Layered Panes, trees var/img, rock var/img

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,36 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
 public class Main {
+
     public static void main(String[] args) {
-    JFrame gameScreen = new JFrame();
-    gameScreen.setSize(750,750);
-    //==================================
-    JPanel startMenu = new JPanel();
-    startMenu.setBackground(Color.blue);
-    //==================================
-        JPanel mainGamePanel = new JPanel();
-        mainGamePanel.setBackground(Color.green);
-        //Adding Start Menu image ==========
-        ImageIcon menuImage = new ImageIcon("StartMenuImage.bmp");
-        menuImage.setImage(menuImage.getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT));
-        JLabel startMenuImage = new JLabel();
-        startMenu.add(startMenuImage);
-        //Adding panels=====================
-        gameScreen.add(startMenu);
-        //==================================
-        startMenu.addMouseListener(new MouseAdapter(){
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                if(e.getY()>=350 & e.getX()>=125 & e.getX()<=625){
-                    System.out.println("Game started");
-                    startMenu.setVisible(false);
-                    gameScreen.add(mainGamePanel);
-                }
-                else{
-                    System.out.println("Game not started");
-                }
+        //================= Rock Image Build =======================
+        ImageIcon rockImage = new ImageIcon("rock.png");
+        rockImage.setImage(rockImage.getImage().getScaledInstance(60,60,Image.SCALE_DEFAULT));
+        //==========================================================
 
-            }
-        });
+        //================= Tree Image Build =======================
+        ImageIcon treeImage = new ImageIcon("tree.png");
+        treeImage.setImage(treeImage.getImage().getScaledInstance(120,120,Image.SCALE_DEFAULT));
+        //==========================================================
+        JLabel rock1 = new JLabel(rockImage);
+        rock1.setBounds(50,50,60,60);
 
-    gameScreen.setVisible(true);
-    System.out.println("testing git push/pull");
+        JLabel tree1 = new JLabel(treeImage);
+        tree1.setBounds(100,100,120,120);
+
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0,0,650,650);
+
+        layeredPane.add(rock1, Integer.valueOf(0));
+        layeredPane.add(tree1, Integer.valueOf(0));
+
+        JFrame frame = new JFrame("JLayeredPane");
+        frame.add(layeredPane);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(500, 500));
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
+
 }
