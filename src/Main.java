25eb35b2afea4d2d62713d86950fa6c51aca5baf
philip.anlauf23 <soccerphil8@yahoo.com>
2023-02-28 11:30:17 -0600

    Added blue background. Added seconds main game panel, added character and 2nd Panel class.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,33 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
+        //Initializing images and misc.
+        ImageIcon treeImage = new ImageIcon("tree.png");
+        treeImage.setImage(treeImage.getImage().getScaledInstance(120,120,Image.SCALE_DEFAULT));
+        ImageIcon rockImage = new ImageIcon("rock.png");
+        rockImage.setImage(rockImage.getImage().getScaledInstance(60,60,Image.SCALE_DEFAULT));
         JLayeredPane layeredPane = new JLayeredPane();
+        ArrayList<JLabel> rocks = new ArrayList<>();
 
-        
+        //=======================
+        //Adding Sprites
 
+        JLabel rock1 = new JLabel(rockImage);
+        rock1.setBounds(100,100,60,60);
+        layeredPane.add(rock1, Integer.valueOf(0));
+
+        //=======================
         JFrame frame = new JFrame("JLayeredPane");
         frame.add(layeredPane);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(new Dimension(1440, 900));
         frame.setLayout(null);
         frame.setVisible(true);
+        //======================
     }
 
 }
