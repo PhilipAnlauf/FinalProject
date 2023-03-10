fd59ca7fae92c344f483e4d0445826b5e02bba94
philip.anlauf23 <soccerphil8@yahoo.com>
2023-03-10 11:31:24 -0600

    Reset code new strategy

--- src/Main.java
+++ src/Main.java
@@ -21,13 +21,15 @@ public class Main {
         ImageIcon playerImage = new ImageIcon("treeAnimation1.png");
         playerImage.setImage(playerImage.getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT));
 
-
         JLayeredPane layeredPane = new JLayeredPane();
         layeredPane.setSize(1440, 900);
+
         //============================================
         //=========== General Game Values ============
         int rockCount = 30;
         int treeCount = 16;
+
+        int playerSpeed = 35;
 
         int maxCreatures = 25;
         int creatureHealth = 30;
@@ -38,7 +40,7 @@ public class Main {
         int axeDamage = 10; //Damage per axe hit
         int sharpenerSpawnRate = 20; //Amount of cycles before new axe sharpener spawn
 
-        int gameInterval = 50*treeSpeed; //in milliseconds
+        int gameInterval = 85; //in milliseconds
         //============================================
         //Adding Sprites
         ArrayList<JLabel> rocks = new ArrayList<>();
@@ -50,7 +52,16 @@ public class Main {
         }
         for(JLabel hold: rocks){
             hold.setBounds((int)(Math.random()*1440),(int)(Math.random()*900),60,60);
-            layeredPane.add(hold, Integer.valueOf(0));
+            layeredPane.add(hold, Integer.valueOf(1));
+        }
+
+        for(int i = 0; i<maxCreatures; i++){
+            monsters.add(new JLabel(treeAnimation1));
+        }
+
+        for(JLabel hold: monsters){
+            hold.setBounds((int)(Math.random()*1440),-200,60,60);
+            layeredPane.add(hold, Integer.valueOf(1));
         }
 
         for(int i = 0; i<treeCount; i++){
@@ -58,18 +69,13 @@ public class Main {
         }
         for(JLabel hold: trees){
             hold.setBounds((int)(Math.random()*1440),(int)(Math.random()*900),120,120);
-            layeredPane.add(hold, Integer.valueOf(2));
-        }
-
-        monsters.add(new JLabel(treeAnimation1));
-        for(JLabel hold: monsters){
-            hold.setBounds(50,50,120,120);
-            layeredPane.add(hold, Integer.valueOf(2));
+            layeredPane.add(hold, Integer.valueOf(3));
         }
 
         JLabel player = new JLabel(playerImage);
-        layeredPane.add(player, Integer.valueOf(2));
+        layeredPane.add(player, Integer.valueOf(3));
         player.setBounds(500, 500,50,50);
+
         //=======================
         JFrame frame = new JFrame("JLayeredPane");
         frame.add(layeredPane);
@@ -87,19 +93,19 @@ public class Main {
             @Override
             public void keyPressed(KeyEvent e) {
               if(e.getKeyCode() == 87){
-                  player.setBounds(player.getX(), player.getY()-25,50,50);
+                  player.setBounds(player.getX(), player.getY()-playerSpeed,50,50);
               }
 
               if(e.getKeyCode() == 65){
-                    player.setBounds(player.getX()-25, player.getY(),50,50);
+                    player.setBounds(player.getX()-playerSpeed, player.getY(),50,50);
               }
 
               if(e.getKeyCode() == 83){
-                    player.setBounds(player.getX(), player.getY()+25,50,50);
+                    player.setBounds(player.getX(), player.getY()+playerSpeed,50,50);
               }
 
               if(e.getKeyCode() == 68){
-                    player.setBounds(player.getX()+25, player.getY(),50,50);
+                    player.setBounds(player.getX()+playerSpeed, player.getY(),50,50);
               }
             }
 
@@ -114,6 +120,14 @@ public class Main {
         //===== Game Start =====
         while(true){
 
+            Point cursor = MouseInfo.getPointerInfo().getLocation();
+
+            if(player.getX() <= cursor.getX()){player.setBounds(player.getX()+playerSpeed, player.getY(),120,120);}
+            if(player.getX() >= cursor.getX()){player.setBounds(player.getX()-playerSpeed, player.getY(),120,120); }
+            if(player.getY() <= cursor.getY()){player.setBounds(player.getX(), player.getY()+playerSpeed,120,120);}
+            if(player.getY() >= cursor.getY()){player.setBounds(player.getX(), player.getY()-playerSpeed,120,120); }
+
+
             for(JLabel hold: monsters){
                 skinChoice = skinChoice*-1;
                 if(skinChoice == -1){
@@ -122,18 +136,10 @@ public class Main {
                 if(skinChoice == 1){
                     hold.setIcon(treeAnimation2);
                 }
-                if(hold.getX() <= player.getX()){
-                    hold.setBounds(hold.getX()+treeSpeed, hold.getY(),120,120);
-                }
-                else{hold.setBounds(hold.getX()-treeSpeed, hold.getY(),120,120); }
-
-                if(hold.getY() <= player.getY()){
-                    hold.setBounds(hold.getX(), hold.getY()+treeSpeed,120,120);
-                }
-                else{hold.setBounds(hold.getX(), hold.getY()-treeSpeed,120,120); }
-
-
-                hold.setBounds(hold.getX()+treeSpeed, hold.getY()+treeSpeed,120,120);
+                if(hold.getX() <= player.getX()){hold.setBounds(hold.getX()+treeSpeed, hold.getY(),120,120);}
+                if(hold.getX() >= player.getX()){hold.setBounds(hold.getX()-treeSpeed, hold.getY(),120,120); }
+                if(hold.getY() <= player.getY()){hold.setBounds(hold.getX(), hold.getY()+treeSpeed,120,120);}
+                if(hold.getY() >= player.getY()){hold.setBounds(hold.getX(), hold.getY()-treeSpeed,120,120); }
             }
 
             try
